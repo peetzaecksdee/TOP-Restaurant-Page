632317d5cc9001205f8e88b6db949962cefbacc8
peetzaecksdee <peetzaecksdee@skiff.com>
2024-02-25 21:34:38 +0700

    Create header function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello guys, welcome to the restaurant page!");
+function createButton(text) {
+  const btn = document.createElement('button')
+  btn.classList.add('btn-nav');
+  btn.textContent = text;
+
+  return btn;
+}
+
+function createHeader() {
+  const header = document.createElement('header');
+  
+  const nav = document.createElement('nav');
+  header.appendChild(nav);
+
+  const homeBtn = createButton('Home');
+  homeBtn.classList.add('active');
+  nav.appendChild(homeBtn);
+
+  const menuBtn = createButton('Menu');
+  nav.appendChild(menuBtn);
+
+  const contactBtn = createButton('Contact');
+  nav.appendChild(contactBtn);
+
+  return header;
+}
+
+function init() {
+  const content = document.getElementById('content');
+
+  content.appendChild(createHeader());
+}
+
+init();
