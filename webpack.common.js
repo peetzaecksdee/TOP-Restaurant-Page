bad92ca6a274f98f8b77cfe7bc13e98de52d99ba
peetzaecksdee <peetzaecksdee@skiff.com>
2024-02-26 13:28:45 +0700

    Finished home page

--- webpack.common.js
+++ webpack.common.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
 }
